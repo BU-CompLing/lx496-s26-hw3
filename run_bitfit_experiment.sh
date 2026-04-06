7390aa76178b439832b09216905e52ed8b70769d
yidinghao <5271164+yidinghao@users.noreply.github.com>
2026-04-06 15:18:44 -0400

    Added sample job script

---
+++ run_bitfit_experiment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+# To change the following settings, see the official SCC instructions at:
+# https://www.bu.edu/tech/support/research/system-usage/running-jobs/submitting-jobs/
+
+#$ -P lx496sh       # This job is for LX 496/796
+#$ -l h_rt=3:00:00  # Reserve the batch node for 3 hours
+#$ -l gpus=1        # Request 1 GPU
+#$ -l gpu_c=7.0     # Request a GPU that is v100 or better
+#$ -N myjobname     # Give your job a name
+#$ -m bea           # Send you an email when the job starts and ends
+#$ -o log.txt       # Save anything printed to the terminal to a file...
+#$ -j y             # ...including both print statements and error messages
+
+module load miniconda
+module load academic-ml/spring-2026
+conda activate spring-2026-pyt
+cd /projectnb/lx496sh/students/your_bu_id
+python train_model.py
