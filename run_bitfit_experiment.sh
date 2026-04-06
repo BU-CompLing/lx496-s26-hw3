f9d77caebbbbe7192d8d41b84a6db27b97796da9
yidinghao <5271164+yidinghao@users.noreply.github.com>
2026-04-06 15:34:04 -0400

    Updated job script

--- run_bitfit_experiment.sh
+++ run_bitfit_experiment.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -l
 
-# To change the following settings, see the official SCC instructions at:
+# Please modify this file as needed, and upload it to your personal SCC folder (or a subdirectory thereof) at:
+# /projectnb/lx496sh/students/your_bu_id
+
+# To submit this job, cd into the folder where you uploaded this file and run:
+# qsub run_bitfit_experiment.sh
+
+# To change the session settings, see the official SCC instructions at:
 # https://www.bu.edu/tech/support/research/system-usage/running-jobs/submitting-jobs/
 
 #$ -P lx496sh       # This job is for LX 496/796
@@ -15,5 +21,5 @@
 module load miniconda
 module load academic-ml/spring-2026
 conda activate spring-2026-pyt
-cd /projectnb/lx496sh/students/your_bu_id
+cd /projectnb/lx496sh/students/your_bu_id  # Modify this with your information
 python train_model.py
